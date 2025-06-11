70735d692ffd35e299725ec6e9a7a93495c068b9
Jiří Kuchyňka (Anty) <anty150@gmail.com>
2025-06-11 21:02:10 +0200

    fix: spacing fixes based on design specs

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import { LangSelector } from './LangSelector';
 export const Navbar = ({ children }: React.PropsWithChildren) => {
   return (
     <div className="navbar">
-      {children}
+      {children ? children : <div />}
       <LangSelector />
     </div>
   );
