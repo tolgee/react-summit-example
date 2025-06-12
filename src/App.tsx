303f3f6895d3222a0f6da7aa2b7337fbe8bf02e8
Jiří Kuchyňka (Anty) <anty150@gmail.com>
2025-06-13 00:23:11 +0200

    refactor: move tolgee instance creation to separate file

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,7 @@
-import {
-  BackendFetch,
-  DevBackend,
-  DevTools,
-  LanguageDetector,
-  Tolgee,
-  TolgeeProvider,
-} from "@tolgee/react";
+import { TolgeeProvider } from "@tolgee/react";
 import { Title } from "./Title.tsx";
 import { Page } from "./components/Page";
 import { QRCode } from "./components/QRCode";
-import { FormatIcu } from "@tolgee/format-icu";
 import { LoadingScreen } from "./components/LoadingScreen.tsx";
 import { DummyOptionsProvider } from "./components/OptionsProvider.tsx";
 import { DevSupport } from "@react-buddy/ide-toolbox";
@@ -17,28 +9,7 @@ import ComponentPreviews from "./dev/previews.tsx";
 import { useInitial } from "./dev/useInitial.ts";
 import { useLanguages } from "./components/useLanguages.tsx";
 import { GlobalLanguagesProvider } from "./components/GlobalLanguagesProvider";
-
-const savedLocale = localStorage.getItem("userLocale") || undefined;
-
-const tolgee = Tolgee()
-  .use(DevTools())
-  .use(FormatIcu())
-  .use(
-    BackendFetch({
-      prefix: import.meta.env.VITE_APP_TOLGEE_CDN_URL,
-    })
-  )
-  .use(LanguageDetector())
-  .use(DevBackend())
-  .init({
-    apiUrl: import.meta.env.VITE_APP_TOLGEE_API_URL,
-    apiKey: import.meta.env.VITE_APP_TOLGEE_API_KEY,
-    projectId: import.meta.env.VITE_APP_TOLGEE_PROJECT_ID,
-    fallbackLanguage: "en",
-    defaultLanguage: "en",
-    availableLanguages: ["en", "cs"],
-    language: savedLocale,
-  });
+import tolgee from "./tolgee";
 
 export const App = () => {
   const { languages, loading } = useLanguages({
---
+++ src/tolgee.ts
@@ -0,0 +1,32 @@
+import {
+  BackendFetch,
+  DevBackend,
+  DevTools,
+  LanguageDetector,
+  Tolgee,
+} from "@tolgee/react";
+import { FormatIcu } from "@tolgee/format-icu";
+
+const savedLocale = localStorage.getItem("userLocale") || undefined;
+
+const tolgee = Tolgee()
+  .use(DevTools())
+  .use(FormatIcu())
+  .use(
+    BackendFetch({
+      prefix: import.meta.env.VITE_APP_TOLGEE_CDN_URL,
+    })
+  )
+  .use(LanguageDetector())
+  .use(DevBackend())
+  .init({
+    apiUrl: import.meta.env.VITE_APP_TOLGEE_API_URL,
+    apiKey: import.meta.env.VITE_APP_TOLGEE_API_KEY,
+    projectId: import.meta.env.VITE_APP_TOLGEE_PROJECT_ID,
+    fallbackLanguage: "en",
+    defaultLanguage: "en",
+    availableLanguages: ["en", "cs"],
+    language: savedLocale,
+  });
+
+export default tolgee;
